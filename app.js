7942893f7c6cb6e2013708164e2fad0ab99730d5
Nikhil-2513 <nikhilkumar_saini@srmap.edu.in>
2024-09-30 16:25:23 +0530

    Removed deprecated Mongoose options

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-// Database connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// Database connection (remove deprecated options)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB', err));
 
